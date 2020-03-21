8fcf30d0218915d97a3e7e86553d62e373ff8599
jehough <teachlife@outlook.com>
2020-03-21 07:39:26 -0400

    adds connection string and user classes

---
+++ netcoregradebook/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace netcoregradebook.Models
+{
+    public class Student : IdentityUser
+    {   
+        [Key]
+        public int StudentId { set; get; }
+        public string FullName { set; get; }
+    }
+}
---
+++ netcoregradebook/Models/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace netcoregradebook.Models
+{
+    public class Teacher
+    {   
+        [Key]
+        public int TeacherId { get; set; }
+        public string FullName { get; set; }
+    }
+}
